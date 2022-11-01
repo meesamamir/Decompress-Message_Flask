beb7eb47d3a0cbba3ff0a3f1c986f007ccf32a5b
Meesam Amir <86166187+meesamamir@users.noreply.github.com>
2022-11-01 06:48:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Author: Meesam Amir Syed
+
+# Running:
+# To run web app, you can directly visit: https://decompress-message.herokuapp.com/
+# To run it locally via terminal, locate this directory ("Decompress-Message_Flask") on your machine, and start terminal
+#  inside it. Use python command:  "python main.py" to compile; this outputs the web app link.
+
 import re
 from flask import Flask, render_template, request
 
@@ -28,14 +35,14 @@ def results():
 
 def uncompressor(input):
 
-    # Check placed to see if input is in the correct format, if not then program does
-    #  not go further
+    # Check placed to see if input is in the correct format, if not then program displays
+    #  error message 
     if (input[0].isdigit() == False) :
         errorMsg = "Message does NOT have a leading integer (1-9). \nValid input examples: 1a2b3c, 3m5a"
         return errorMsg
 
-    # Using regex we can extract a group of an integer and an alphanumeric character
-    # We can extend the functionality of this by extracting a group of an integer 
+    # Using Regular Expressions (regex) we can extract a group of an integer and an alphanumeric character
+    # Functionality of this has been extended to extracting a group of an integer 
     #  followed a string of any length
     #   example1: '1a2b3c' = ['1a', '2b', '3c] 
     #   example2: '3mno5abcd6xyz' = ['3mno', '5abcd', '6xyz']
@@ -47,7 +54,7 @@ def uncompressor(input):
     for i in match:
         # After the 'match' array is created, iterate on each element and by using 
         #  string manipulations (slicing) we can repeat the alphanumeric part 'n' times 
-        #  (according to the leading integer) and join the strings using the '+' character
+        #  (according to the leading integer) and finally, join the strings using the '+' character and output
         decompString = decompString + (i[1:]*int(i[0])) 
             
     return input + " => " + decompString
@@ -56,5 +63,4 @@ def uncompressor(input):
 if __name__ == '__main__':
     app.run(debug=True)  # update flask server with changes we make
 
-
-
+    
